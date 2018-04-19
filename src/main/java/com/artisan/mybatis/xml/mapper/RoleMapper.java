a8b568bfe501de95e3caa1636c603534c7f0c315
Mr.Yang <815150141@qq.com>
2018-04-19 00:30:36 -0400

    MyBatis-07MyBatis注解方式之@Select

--- src/main/java/com/artisan/mybatis/xml/mapper/RoleMapper.java
+++ src/main/java/com/artisan/mybatis/xml/mapper/RoleMapper.java
@@ -1,5 +1,126 @@
 package com.artisan.mybatis.xml.mapper;
 
-public interface RoleMapper {
+import java.util.List;
 
-}
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import com.artisan.mybatis.xml.domain.SysRole;
+
+/**
+ * 
+ * 
+ * @ClassName: RoleMapper
+ * 
+ * @Description: 这里通过注解的方式演示用法，一般情况下不推荐使用注解的方式
+ * 
+ * @author: Mr.Yang
+ * 
+ * @date: 2018年4月17日 下午8:34:54
+ */
+public interface RoleMapper {
+	/**
+	 * 
+	 * 
+	 * @Title: selectSysRoleById
+	 * 
+	 * @Description: 采用数据库字段别名和实体类属性同名的方式映射
+	 * 
+	 * @param roleId
+	 * @return
+	 * 
+	 * @return: SysRole
+	 */
+	
+	@Select({ "SELECT "
+				+ " a.id, "
+				+ " a.role_name roleName, "
+				+ " a.enabled, "
+				+ " a.create_by createBy, "
+				+ " a.create_time createTime "
+			+ " FROM "
+				+ " sys_role a "
+			+ " WHERE "
+			+ " a.id = #{roleId}" })
+	SysRole selectSysRoleById(Long roleId);
+			
+	
+	/**
+	 * 
+	 * 
+	 * @Title: selectSysRoleById2
+	 * 
+	 * @Description: 采用 MyBatis全局配置文件设置 mapUnderscoreToCamelCase 映射
+	 *               通过配置这个属性为true可以自动将下画线方式命名的数据库列映射到java对象驼峰式命名属性中
+	 * 
+	 * @param roleId
+	 * @return
+	 * 
+	 * @return: SysRole
+	 */
+	@Select({ "SELECT "
+			+ " a.id, "
+			+ " a.role_name , "
+			+ " a.enabled, "
+			+ " a.create_by , "
+			+ " a.create_time "
+		+ " FROM "
+			+ " sys_role a "
+		+ " WHERE "
+		+ " a.id = #{roleId}" })
+	SysRole selectSysRoleById2(Long roleId);
+	
+	
+	
+	
+	/**
+	 * 
+	 * 
+	 * @Title: selectSysRoleById3
+	 * 
+	 * @Description: 通过ResultMap的方式实现数据库列到java对象的映射
+	 * 
+	 * @param roleId
+	 * @return
+	 * 
+	 * @return: SysRole
+	 */
+
+	@Results(id = "roleResultMap", value ={ 
+		@Result(property = "id", column = "id", id = true),
+ @Result(property = "roleName", column = "role_name"), @Result(property = "enabled", column = "enabled"),
+			@Result(property = "createBy", column = "create_by"), @Result(property = "createTime", column = "create_time")
+	})
+	@Select({ "SELECT "
+			+ " a.id, "
+			+ " a.role_name , "
+			+ " a.enabled, "
+			+ " a.create_by , "
+			+ " a.create_time "
+		+ " FROM "
+			+ " sys_role a "
+		+ " WHERE "
+		+ " a.id = #{roleId}" })
+	SysRole selectSysRoleById3(Long roleId);
+
+
+	/**
+	 * 
+	 * 
+	 * @Title: selectSysRoleById4
+	 * 
+	 * @Description: 通过改方法演示在mybatis3.3.1版本及其之后的版本 通过@ResultMap应用带有id的@Results
+	 * 
+	 * @param roleId
+	 * @return
+	 * 
+	 * @return: SysRole
+	 */
+
+	@ResultMap("roleResultMap")
+	@Select({
+ "SELECT  a.id, a.role_name,  a.enabled, a.create_by ,  a.create_time  FROM  sys_role a " })
+	List<SysRole> selectAllSysRole();
+}	
---
+++ src/test/java/com/artisan/mybatis/xml/mapper/RoleMapperTest.java
@@ -0,0 +1,116 @@
+package com.artisan.mybatis.xml.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.artisan.mybatis.xml.domain.SysRole;
+
+public class RoleMapperTest extends BaseMapperTest {
+
+	private static final Logger logger = Logger.getLogger(RoleMapperTest.class);
+
+	SqlSession sqlSession;
+
+	@Test
+	public void selectSysRoleByIdTest() {
+
+		logger.info("selectSysRoleByIdTest");
+		try {
+			// 获取SqlSession
+			sqlSession = getSqlSession();
+			// 获取接口
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			// 调用接口方法
+			SysRole sysRole = roleMapper.selectSysRoleById((long) 1);
+			// 期待不为空
+			Assert.assertNotNull(sysRole);
+			// 期望为 roleName="管理员"
+			Assert.assertEquals("管理员", sysRole.getRoleName());
+
+			logger.info("sysRole Info:" + sysRole);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	/**
+	 * 
+	 * 
+	 * @Title: selectSysRoleByIdTest2
+	 * 
+	 * @Description: 测试的时候需要将 <setting name="mapUnderscoreToCamelCase"
+	 *               value="true"/> 放开
+	 * 
+	 * 
+	 * @return: void
+	 */
+	@Test
+	public void selectSysRoleByIdTest2() {
+		logger.info("selectSysRoleByIdTest2");
+		try {
+			// 获取SqlSession
+			sqlSession = getSqlSession();
+			// 获取接口
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			// 调用接口方法
+			SysRole sysRole = roleMapper.selectSysRoleById2((long) 2);
+			// 期待不为空
+			Assert.assertNotNull(sysRole);
+			// 期望为 roleName="普通用户"
+			Assert.assertEquals("普通用户", sysRole.getRoleName());
+
+			logger.info("sysRole Info:" + sysRole);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	@Test
+	public void selectSysRoleByIdTest3() {
+		logger.info("selectSysRoleByIdTest3");
+		try {
+			// 获取SqlSession
+			sqlSession = getSqlSession();
+			// 获取接口
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			// 调用接口方法
+			SysRole sysRole = roleMapper.selectSysRoleById3((long) 2);
+			// 期待不为空
+			Assert.assertNotNull(sysRole);
+			// 期望为 roleName="普通用户"
+			Assert.assertEquals("普通用户", sysRole.getRoleName());
+
+			logger.info("sysRole Info:" + sysRole);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	@Test
+	public void selectAllSysTest() {
+		logger.info("selectAllSysTest");
+		try {
+			// 获取SqlSession
+			sqlSession = getSqlSession();
+			// 获取接口
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			// 调用接口方法
+			List<SysRole> sysRoleList = roleMapper.selectAllSysRole();
+			// 期待不为空
+			Assert.assertNotNull(sysRoleList);
+			// 期望为sysRoleList > 0
+			Assert.assertTrue(sysRoleList.size() > 0);
+
+			for (SysRole sysRole2 : sysRoleList) {
+				logger.info("sysRole Info:" + sysRole2);
+			}
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+}
