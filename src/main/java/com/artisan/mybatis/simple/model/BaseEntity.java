61b125114f70adc36725ac01c2a3e2451a54b9e2
Mr.Yang <815150141@qq.com>
2018-04-27 22:44:17 -0400

    MyBatis代码生成器（逆向工程）MBG使用

---
+++ src/main/java/com/artisan/mybatis/simple/model/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.artisan.mybatis.simple.model;
+
+public class BaseEntity {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
